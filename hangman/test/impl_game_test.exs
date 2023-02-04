6916b13619fe7455e2c725365f2959088e9c20a9
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2023-02-04 07:02:58 +0100

    Add game guess logic for guessing and already guessed guesses

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -23,4 +23,30 @@ defmodule ImplGameTest do
     lowercase_letters = game.letters |> Enum.map(fn x -> String.downcase(x) end)
     assert game.letters == lowercase_letters
   end
+
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game()
+      game = Map.put(game, :game_state, state)
+      {new_game, _tally} = Game.make_move(game, "x")
+      assert game == new_game
+    end
+  end
+
+  test "duplicate letter is reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "letters are being recorded" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "y")
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
+  end
 end
