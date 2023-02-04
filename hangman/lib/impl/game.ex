61d41ea9091dfdc0acefe9382918563eace02d85
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2023-02-04 06:16:17 +0100

    Add tests

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,7 +1,9 @@
 defmodule Hangman.Impl.Game do
+  alias Hangman.Type
+
   @type t :: %__MODULE__{
           turns_left: integer,
-          game_state: Hangman.state(),
+          game_state: Type.state(),
           letters: list(String.t()),
           used: MapSet.t(String.t())
         }
@@ -12,9 +14,15 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
+  @spec new_game() :: t
   def new_game do
+    new_game(Dictionary.random_word())
+  end
+
+  @spec new_game(String.t()) :: t
+  def new_game(word) do
     %__MODULE__{
-      letters: Dictionary.random_word() |> String.codepoints()
+      letters: word |> String.codepoints()
     }
   end
 end
